090b42a6763b763ce3da70f34be72b617d70de40
小野　総司郎 <ssosksono@gmail.com>
2020-11-25 23:16:11 +0900

    ransackの導入

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,12 @@
 class Song < ApplicationRecord
+  belongs_to :user
+
+  # アクティブハッシュのアソシエーション
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :gender
+  belongs_to_active_hash :interval
+  belongs_to_active_hash :genre
+  belongs_to_active_hash :populur
+
+  # アクティブハッシュのアソシエーション
 end
