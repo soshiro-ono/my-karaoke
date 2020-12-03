7f1502c52ba70e143d12b7e607913a96b703af8d
小野　総司郎 <ssosksono@gmail.com>
2020-12-03 17:22:09 +0900

    songのテストコード

---
+++ spec/factories/songs.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :song do
+    artist             {"バンプ"}
+    title              {"天体観測"}
+    genre_id           {2}
+    interval_id        {2}
+    populur_id         {2}
+    gender_id          {2}
+  end
+end
